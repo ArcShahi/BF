a27be4e9763dc2120eff55e3d37c045254fe4f52
Shahi <90377780+ArcShahi@users.noreply.github.com>
2026-08-12 09:32:33 +0530

    Re-wrote bf++.cpp
    Single function now. Removed vector tape with `std::array` and tape size increased to 3K

--- bf++.cpp
+++ bf++.cpp
@@ -3,30 +3,58 @@
 #include <string>
 #include <filesystem>
 #include <unordered_map>
+#include <array>
 
 namespace fs = std::filesystem;
 
-namespace bf {
+namespace {
 
-	void interpreter(std::string&& program) noexcept {
-		std::vector<uint8_t> tape(100, 0);
+	[[nodiscard]]int interpreter(const fs::path& path)noexcept {
+
+		//  Preprocesser
+		if (!fs::is_regular_file(path) || path.extension() != ".bf") {
+			std::cout << "bf++: srcfile not found" << path.string();
+			return 1;
+		}
+
+		std::ifstream file(path);
+		if (!file.is_open()) {
+			std::cerr << "Can't open file";
+			return 1;
+		}
+
+		std::string program{ std::istreambuf_iterator<char>(file), std::istreambuf_iterator<char>() };
+		file.close();
+
+		// Filter comments
+		program.erase(std::remove_if(program.begin(), program.end(),
+					   [](char c) {
+						   static const std::string ins{ "+-<>.,[]" };
+						   return ins.find(c) == std::string::npos;
+					   }),
+		    program.end());
+
+
+		std::array<uint8_t, 3000> tape{};
 		size_t arrow{ 0 };
 
-		// IIFE : for loop index lookup
+		// IIFE : Loop Lookup table
 		const auto loop_lookup = [&]() {
 			std::vector<int> symbol_stk{};
-			std::unordered_map<int, int> loop_lookup{};
+			std::unordered_map<int, int> lookup{};
 			for (int i{ 0 }; i < program.length(); ++i) {
 				if (program[i] == '[')
 					symbol_stk.push_back(i);
 				else if (program[i] == ']') {
-					loop_lookup[symbol_stk.back()] = i;
-					loop_lookup[i] = symbol_stk.back();
+					lookup[symbol_stk.back()] = i;
+					lookup[i] = symbol_stk.back();
 					symbol_stk.pop_back();
 				}
 			}
-			return loop_lookup;
+			return lookup;
 		}();
+
+		// Execution
 
 		for (int ip{ 0 }; ip < program.length(); ++ip) {
 			switch (program[ip]) {
@@ -36,87 +64,39 @@ namespace bf {
 			case '-':
 				--tape[arrow];
 				break;
-			case '>': {
+			case '>':
 				++arrow;
-				if (arrow >= tape.size()) {
-					std::cerr << "Tape overflow\n";
-					return;
-				}
 				break;
-			}
-
-			case '<': {
+			case '<':
 				--arrow;
-				if (arrow < 0) {
-					std::cerr << "Tape pointer out of bounds\n";
-				}
 				break;
-			}
-
-			case '.': {
+			case '.':
 				std::cout << static_cast<char>(tape[arrow]);
 				break;
-			}
-
-			case ',': {
-				char input{};
-				std::cin >> input;
-				tape[arrow] = static_cast<uint8_t>(input);
+			case ',':
+				std::cin >> tape[arrow];
 				break;
-			}
-			case '[': {
-				if (!tape[arrow])
-					// Skip to matching ']'
+			case '[':
+				if (!tape[arrow]) 
+					ip = loop_lookup.at(ip);
+				break;
+			case ']':
+				if (tape[arrow]) 
 					ip = loop_lookup.at(ip);
 				break;
 			}
-
-			case ']': {
-				// loop back to '[' if current tape pointer is non-zero
-				if (tape[arrow])
-					ip = loop_lookup.at(ip);
-
-				break;
-			}
-			default:
-				break;
-			}
 		}
-	}
-
-	int preprocessor(const fs::path& path) noexcept {
-		if (!fs::is_regular_file(path) || path.extension() != ".bf") {
-			std::cout << "NO BRAINFUCK SOURCE FOUND -_- " << path.string();
-			return 1;
-		}
-
-		std::ifstream srcfile{ path };
-	
-		if (!srcfile.is_open()) {
-			std::cerr << "Can't open file";
-			return 1;
-		}
-		const std::string instructions{ "+-<>.,[]" };
-		std::string program{};
-		program.reserve(100);
-
-		char token{};
-		while (srcfile.get(token))
-			if (instructions.find(token) != std::string::npos)
-				program.push_back(token);
-
-		srcfile.close();
-		interpreter(std::move(program));
 		return 0;
 	}
+
 
 }
 
 int main(int argc, char* argv[]) {
-
 	if (argc == 1) {
-		std::cout << "USAGE: bf++ <src.bf>";
+		std::cerr << "bf++: error: no input file";
 		return 1;
 	}
-	bf::preprocessor(argv[1]);
+	 
+	return interpreter(argv[1]);
 }
